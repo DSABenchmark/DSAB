3431b9bcb8a3079f1e0ce2fa6e108ae15feedd3f
Yuol96 <huangyc96@gmail.com>
2018-10-29 20:16:42 +0800

    add download file for default datasets

---
+++ download.sh
@@ -0,0 +1,3 @@
+wget -O dataset/formatted00.dat "http://dsabdatasets.oss-cn-beijing.aliyuncs.com/formatted00.dat"
+wget -O dataset/kosarak.dat "http://dsabdatasets.oss-cn-beijing.aliyuncs.com/kosarak.dat" 
+wget -O dataset/webdocs00.dat "http://dsabdatasets.oss-cn-beijing.aliyuncs.com/webdocs00.dat"
